f098d51757c57ae6da05fb7867ec3a5e940134cd
Elijah Davis <eli.davis1104@gmail.com>
2025-07-09 11:01:26 -0400

    updated bar scanning functionality

--- frontend/src/pages/InventoryPage.tsx
+++ frontend/src/pages/InventoryPage.tsx
@@ -1,16 +1,57 @@
 import ItemScanner from "../components/ItemScanner";
 import { useState } from "react";
 import { cn } from "@udecode/cn";
+import BarcodeScanner from "react-qr-barcode-scanner";
+import ModalContainer from "../components/ModalContainer";
+
+type PantryItem = string;
 
 const CameraView = () => {
   return <></>;
 };
 
-const BarcodeView = () => {
-  const clickHandler = () => {};
+type BarcodeViewProps = {
+  setPantryItems: React.Dispatch<React.SetStateAction<string[]>>;
+  pantryItems: PantryItem[];
+};
+
+const BarcodeView = ({ pantryItems, setPantryItems }: BarcodeViewProps) => {
+  const [barcode, setBarcode] = useState<null | string>(null);
+  const [displayCamera, setDisplayCamera] = useState(false);
+  const clickHandler = () => {
+    setDisplayCamera(true);
+  };
+
+  const updateHandler = (err: any, result: any) => {
+    if (result) {
+      setBarcode(result.text);
+      if (!pantryItems.includes(result.text)) {
+        setPantryItems(pantryItems.concat(result.text));
+      }
+      // need to fetch ingredient info and add to list
+    }
+  };
 
   return (
     <>
+      {displayCamera && (
+        <ModalContainer close={() => setDisplayCamera(false)}>
+          <div className="flex items-center py-30 flex-col">
+            <h1 className="font-bold text-3xl mb-3">
+              Make sure the barcode is visible for your scan
+            </h1>
+            <BarcodeScanner width={500} height={500} onUpdate={updateHandler} />
+            <ul className="text-blue-700 text-lg">
+              {pantryItems.map((item) => (
+                <li key={item}>{item}</li>
+              ))}
+            </ul>
+            <button className="text-white bg-blue-700 font-bold px-2 py-3 rounded-3xl mt-3">
+              Close Camera
+            </button>
+          </div>
+        </ModalContainer>
+      )}
       <div className="flex flex-col items-center">
         <div className="border border-black w-[360px] h-[200px] rounded-xl"></div>
         <h2 className="font-semibold">Capture Item</h2>
@@ -33,8 +74,12 @@ const ManualView = () => {
   return <></>;
 };
 
+const ItemsList = () => {};
+
 const InventoryPage = () => {
   const [mode, setMode] = useState<"camera" | "barcode" | "manual">("barcode");
+  // will store all scanned / manually added items
+  const [pantryItems, setPantryItems] = useState<PantryItem[]>([]);
 
   return (
     <>
@@ -70,8 +115,18 @@ const InventoryPage = () => {
         </button>
       </div>
       {mode === "manual" && <ManualView />}
-      {mode === "barcode" && <BarcodeView />}
+      {mode === "barcode" && (
+        <BarcodeView
+          setPantryItems={setPantryItems}
+          pantryItems={pantryItems}
+        />
+      )}
       {mode === "camera" && <CameraView />}
+      <ul>
+        {pantryItems.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
     </>
   );
 };
