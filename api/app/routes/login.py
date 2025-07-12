26f043b318c2548632ac52b30e3fa88f922a3f45
nick-socci <nick.socci@gmail.com>
2025-07-12 14:51:26 -0400

    added login/registration api

---
+++ api/app/routes/login.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import create_access_token
+from werkzeug.security import check_password_hash, generate_password_hash
+from app.models.user import User
+from app import db
+
+
+login_bp = Blueprint('login', __name__)
+
+
+@login_bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+
+    if (not user or
+            not check_password_hash(user.password_hash, data['password'])):
+        return jsonify({"msg": "Invalid Username and/or Password"}), 401
+
+    token = create_access_token(identity=user.id)
+    return jsonify({"access_token": token}), 200
+
+
+@login_bp.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+
+    username = data.get('username')
+    password = data.get('password')
+
+    if User.query.filter_by(username=username).first():
+        return jsonify({"msg": "Username already exists"}), 409
+
+    hashed_password = generate_password_hash(password)
+    new_user = User(username=username, password_hash=hashed_password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"msg": "User created successfully"}), 201
--- api/config.py
+++ api/config.py
@@ -4,4 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'my_test_key'
+
