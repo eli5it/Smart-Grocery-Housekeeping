8eb9591fd7db754fb3a751d2406444e038244171
Elijah Davis <eli.davis1104@gmail.com>
2025-07-19 21:55:07 -0400

    removed unecessary test config

--- api/config.py
+++ api/config.py
@@ -1,15 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'my_test_key'
-
-# create as subclass to share common values
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'test-secret-key'
-    TESTING = True
