62bd9b3ea8bb10b5ba3424450d7317c72dd28ca6
Elijah Davis <eli.davis1104@gmail.com>
2025-07-31 19:25:37 -0400

    add delete functionality

--- frontend/src/components/PantryTable.tsx
+++ frontend/src/components/PantryTable.tsx
@@ -2,25 +2,40 @@ import { useState } from "react";
 import type { PantryEntry, PantryEntryByProductName } from "../lib/types";
 import { cn } from "@udecode/cn";
 import { X, Pencil } from "lucide-react";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { queryClient } from "../lib/queryClient";
 
 type SubEntryProps = {
   entry: PantryEntry;
+  deleteEntry: (entryId: number) => void;
 };
-const SubEntry = ({ entry }: SubEntryProps) => {
+const SubEntry = ({ entry, deleteEntry }: SubEntryProps) => {
+  const [editing, setEditing] = useState(false);
   return (
     <tr className="border-t border-gray-500">
       <td className="px-10 py-2">{entry.product_name}</td>
       <td className="px-5 py-2">{entry.ingredient.name}</td>
       <td className="px-5 py-2">{entry.expiration_date}</td>
-      <td>Edit</td>
+      <td className="px-5">
+        <button onClick={() => setEditing(!editing)}>
+          {!editing ? <Pencil /> : <X />}
+        </button>
+      </td>
+      <td className="px-5">
+        <button onClick={() => deleteEntry(entry.id)}>
+          <X />
+        </button>
+      </td>
     </tr>
   );
 };
 
 type PantryTableEntryProps = {
   entries: PantryEntry[];
+  deleteEntry: (entryId: number) => void;
 };
-const PantryTableEntry = ({ entries }: PantryTableEntryProps) => {
+const PantryTableEntry = ({ entries, deleteEntry }: PantryTableEntryProps) => {
   const [expanded, setExpanded] = useState(false);
   const [editing, setEditing] = useState(false);
   // this is emergency error handling, entries should always have a non-zero length
@@ -48,11 +63,20 @@ const PantryTableEntry = ({ entries }: PantryTableEntryProps) => {
         </td>
         <td className="px-5 py-2">{entries[0].ingredient.name}</td>
         <td className="px-5 py-2">{entries[0].expiration_date}</td>
-        <td className="px-5">{!editing ? <Pencil /> : <X />}</td>
+        <td className="px-5">
+          <button onClick={() => setEditing(!editing)}>
+            {!editing ? <Pencil /> : <X />}
+          </button>
+        </td>
+        <td>
+          <button onClick={() => deleteEntry(entries[0].id)} className="px-5">
+            <X />
+          </button>
+        </td>
       </tr>
       {expanded &&
         entries.slice(1).map((entry) => {
-          return <SubEntry entry={entry}></SubEntry>;
+          return <SubEntry deleteEntry={deleteEntry} entry={entry}></SubEntry>;
         })}
     </>
   );
@@ -62,6 +86,22 @@ type PantryTableProps = {
   pantryEntries: PantryEntryByProductName;
 };
 const PantryTable = ({ pantryEntries }: PantryTableProps) => {
+  const deletePantryEntry = (entryId: number) => {
+    const token = localStorage.getItem("access_token");
+    return axios.delete(`/api/pantry/${entryId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  };
+
+  const mutation = useMutation({
+    mutationFn: deletePantryEntry,
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ["pantry"] });
+    },
+  });
   return (
     <>
       <div className="px-2 py-4 max-w-[800px]">
@@ -81,11 +121,17 @@ const PantryTable = ({ pantryEntries }: PantryTableProps) => {
               <th className="px-5 py-2 text-left" scope="col">
                 Edit
               </th>
+              <th className="px-5 py-2 text-left" scope="col">
+                Delete
+              </th>
             </tr>
           </thead>
           <tbody>
             {Object.keys(pantryEntries).map((product_name) => (
-              <PantryTableEntry entries={pantryEntries[product_name]} />
+              <PantryTableEntry
+                deleteEntry={mutation.mutate}
+                entries={pantryEntries[product_name]}
+              />
             ))}
           </tbody>
         </table>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Toast } from "radix-ui";
+import { queryClient } from "./lib/queryClient";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance and query client
 const router = createRouter({ routeTree });
-const queryClient = new QueryClient();
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
