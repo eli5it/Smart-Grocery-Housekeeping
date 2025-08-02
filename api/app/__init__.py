9266d20836da777a1e63e987c3b3fe4fc59bed67
nick-socci <nick.socci@gmail.com>
2025-08-02 12:32:26 -0400

    Added /reports route, schema, and tests

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -52,6 +52,7 @@ def create_app(test_config=None):
     from app.routes.pantry_entry import pantry_entry_bp
     from app.routes.vision import vision_bp
     from app.routes.recipe import recipe_bp
+    from app.routes.reports import reports_bp
 
     app.register_blueprint(ingredient_bp)
     app.register_blueprint(errors_bp)
@@ -59,8 +60,9 @@ def create_app(test_config=None):
     app.register_blueprint(login_bp, url_prefix='/api')
     app.register_blueprint(pantry_bp, url_prefix='/api')
     app.register_blueprint(pantry_entry_bp)
-    app.register_blueprint(vision_bp)    
+    app.register_blueprint(vision_bp)
     app.register_blueprint(recipe_bp, url_prefix='/api')
+    app.register_blueprint(reports_bp, url_prefix='/api')
 
     return app
 
