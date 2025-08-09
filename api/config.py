d563b02df6b8a560226b8752d726e76f24b93456
eli5it <eli.davis1104@gmail.com>
2025-08-09 17:55:50 -0400

    updated database path

--- api/config.py
+++ api/config.py
@@ -1,8 +1,8 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+print(os.environ.get('DATABASE_URL'))
 
 class Config:
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    #     f"sqlite:///{os.path.join(basedir, 'app.db')}"
-    SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/app.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        f"sqlite:///{os.path.join(basedir, 'app.db')}"
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'my_test_key'
